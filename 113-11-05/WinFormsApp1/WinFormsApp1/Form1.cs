aa0e0ffd4596ba39f5bb71bce540ba56edf06dee
hWARD-source <a113221026@mail.shu.edu.tw>
2024-11-05 17:50:42 +0800

    113-11-05

---
+++ 113-11-05/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
